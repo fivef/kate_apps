67da625fc319abbae77e849310bbacd651982ced
pfiffnst <pfiffnst@hs-weingarten.de>
2013-09-19 15:12:19 +0200

    Minor test changes

--- python_pick_and_place_test/src/pick_and_place.py
+++ python_pick_and_place_test/src/pick_and_place.py
@@ -8,8 +8,13 @@ import rospy
 import sys
 
 from moveit_commander import MoveGroupCommander
+
+
 from math import pi
+from math import atan2
+import tf
 from tf.transformations import quaternion_from_euler
+from tf.transformations import euler_from_quaternion
 
 
 class PickAndPlace():
@@ -22,11 +27,12 @@ class PickAndPlace():
         p = PoseStamped()
         p.header.frame_id = "/katana_base_link"
         p.pose.position.x = 0.4287
-        p.pose.position.y = -0.0847
+        #p.pose.position.y = -0.0847
+        p.pose.position.y = 0.0
         p.pose.position.z = 0.4492
         
         
-        q = quaternion_from_euler(0, 0, 0)
+        q = quaternion_from_euler(2, 0, 0)
         
         p.pose.orientation.x = q[0]
         p.pose.orientation.y = q[1]
@@ -39,7 +45,7 @@ class PickAndPlace():
         print "Planning frame: " ,group.get_planning_frame()
         print "Pose reference frame: ",group.get_pose_reference_frame()
         
-       # group.set_pose_reference_frame("katana_base_link")
+        #group.set_pose_reference_frame("katana_base_link")
 
         print "RPy target: 0,0,0"
         #group.set_rpy_target([0, 0, 0],"katana_gripper_tool_frame")
@@ -48,11 +54,21 @@ class PickAndPlace():
         group.set_pose_target(p, "katana_gripper_tool_frame")
         
         group.go()
-        print "Current rpy: " , group.get_current_rpy("katana_motor5_wrist_roll_link")
+        print "Current rpy: " , group.get_current_rpy("katana_gripper_tool_frame")
         
         
-        
-       
+    def replace_yaw_angle_with_reachable_value(self, pose_stamped):
+      #print "Original pose" , pose_stamped
+      euler_orientation = list(euler_from_quaternion([pose_stamped.pose.orientation.x,pose_stamped.pose.orientation.y,pose_stamped.pose.orientation.z,pose_stamped.pose.orientation.w]))
+      euler_orientation[2] = atan2(pose_stamped.pose.position.y, pose_stamped.pose.position.x)
+      
+      quaternion = quaternion_from_euler(euler_orientation[0], euler_orientation[1], euler_orientation[2])
+      pose_stamped.pose.orientation.x = quaternion[0]
+      pose_stamped.pose.orientation.y = quaternion[1]
+      pose_stamped.pose.orientation.z = quaternion[2]
+      pose_stamped.pose.orientation.w = quaternion[3]
+      
+      #print "Adjusted yaw: " , pose_stamped
         
         
         
